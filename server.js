c9e34b0c7af4faeed18c9c006f1bfc31d2c0bcc4
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2023-01-08 11:39:54 +0300

    Project Backend Database Process Signin Finish

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const e = require("express");
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    port: 5432,
+    password: "volkan123",
+    database: "smartBrainDB",
+  },
+});
 
 const database = {
   users: [
@@ -28,24 +42,25 @@ const database = {
 };
 
 app.post("/signin", (req, res) => {
-  bcrypt.compare(
-    "bacon",
-    "$2a$10$57R5PLXycGHB.wvUmcbVZOha4tqQmdx.GsP2c/urZQ/IjBUMzhsHW",
-    (err, res) => {
-      console.log("First Guess", res);
-    }
-  );
-  bcrypt.compare(
-    "veggies",
-    "$2a$10$57R5PLXycGHB.wvUmcbVZOha4tqQmdx.GsP2c/urZQ/IjBUMzhsHW",
-    (err, res) => {
-      console.log("second Guess", res);
-    }
-  );
+  // bcrypt.compare(
+  //   "bacon",
+  //   "$2a$10$57R5PLXycGHB.wvUmcbVZOha4tqQmdx.GsP2c/urZQ/IjBUMzhsHW",
+  //   (err, res) => {
+  //     console.log("First Guess", res);
+  //   }
+  // );
+  // bcrypt.compare(
+  //   "veggies",
+  //   "$2a$10$57R5PLXycGHB.wvUmcbVZOha4tqQmdx.GsP2c/urZQ/IjBUMzhsHW",
+  //   (err, res) => {
+  //     console.log("second Guess", res);
+  //   }
+  // );
   if (
     req.body.email === database.users[0].email &&
-    req.body.password === database / users[0].password
+    req.body.password === database.users[0].password
   ) {
+    res.json(database.users[0]);
     res.json("succesful");
   } else {
     res.status(404).json("error loggin in");
@@ -54,47 +69,44 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, password, name } = req.body;
-  //!   bcrypt.hash(password, null, null, (err, hash) => {
-  //     console.log(hash);
-  //   });
-  database.users.push({
-    id: 3,
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.send(database.users[database.users.length - 1]);
+  db("users")
+    .returning("*")
+    .insert({ email: email, name: name, joined: new Date() })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to register");
+    });
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id == id) {
-      found = true;
-      return res.json(user);
-    }
-    if (!found) {
-      res.status(404).json("Not Found");
-    }
-  });
+  db.select("*")
+    .from("users")
+    .where({
+      id: id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json("Not Found");
+      }
+    })
+    .catch((err) => res.status(400).json("err getting user"));
 });
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id == id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-    if (!found) {
-      res.status(404).json("Not Found");
-    }
-  });
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
 });
 
 app.get("/", (req, res) => {
